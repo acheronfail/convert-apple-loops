fae163c26505c0d1d02338c7e3365e0a099c864a
acheronfail <acheronfail@gmail.com>
2020-05-19 15:41:36 +1000

    chore: update comments

--- convert-apple-loops.sh
+++ convert-apple-loops.sh
@@ -4,14 +4,15 @@ loop_dir="/Library/Audio/Apple Loops/Apple"
 # Create a temporary directory on the Desktop.
 mkdir -p "$temp_dir" && cd "$temp_dir"
 
-# Find every .caf file in the loops directory, convert it to .aiff, maintaining directory structure:
+# Find every .caf file in the loops directory, convert it to .aiff, maintaining directory structure.
+# NOTE: Ableton Live requires that AIFF files be in BEI16 format (max 16 bits).
 echo "Finding and converting .caf loops..."
 fd . \
     --extension .caf \
     -x bash -c 'mkdir -p ".$(dirname "{}")" && afconvert -f AIFF -d BEI16 "{}" ".{}.aiff" && rename "s/.caf//" ".{}.aiff"' \
     "$loop_dir"
 
-# Move converted loops to the Desktop
+# Move converted loops to the Desktop.
 echo "Cleaning up..."
 mv ".$loop_dir" "~/Desktop/Apple Loops"
 rm -rf "$temp_dir"
