fae1624638fed1fe5b0a5f60fd583a547bb94d16
acheronfail <acheronfail@gmail.com>
2020-05-19 15:46:40 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# `convert-apple-loops`
+
+A simple script that converts the Apple Loops from Garageband from `caf` into `aiff` so other programs (like Ableton) can use them.
+
+### Requirements
+
+* macOS
+* Garageband installed, with the loops downloaded
+* [`fd`](https://github.com/sharkdp/fd) (can be installed via `brew install fd`)
+* About ~30GB of free disk space
+
+### Usage
+
+```bash
+# Clone this repository.
+git clone git@github.com:acheronfail/convert-apple-loops.git
+cd convert-apple-loops
+
+# Run the script!
+bash convert-apple-loops.sh
+```
---
+++ convert-apple-loops.sh
@@ -0,0 +1,19 @@
+temp_dir="~/Desktop/convert-apple-loops-temp"
+loop_dir="/Library/Audio/Apple Loops/Apple"
+
+# Create a temporary directory on the Desktop.
+mkdir -p "$temp_dir" && cd "$temp_dir"
+
+# Find every .caf file in the loops directory, convert it to .aiff, maintaining directory structure:
+echo "Finding and converting .caf loops..."
+fd . \
+    --extension .caf \
+    -x bash -c 'mkdir -p ".$(dirname "{}")" && afconvert -f AIFF -d BEI16 "{}" ".{}.aiff" && rename "s/.caf//" ".{}.aiff"' \
+    "$loop_dir"
+
+# Move converted loops to the Desktop
+echo "Cleaning up..."
+mv ".$loop_dir" "~/Desktop/Apple Loops"
+rm -rf "$temp_dir"
+
+echo "Complete! All Apple Loops can be found at: ~/Desktop/Apple Loops"
